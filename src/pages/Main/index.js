8d6565f9f54b8f5c69ae9cb3ea63634b1000ed95
Samuel Terra <samuelterra22@hotmail.com>
2019-08-08 11:19:54 -0300

    Adapt tabs and create menu

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,93 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import { Animated } from 'react-native'
+import { PanGestureHandler, State } from 'react-native-gesture-handler'
 
 import Header from '~/components/Header'
+import Menu   from '~/components/Menu'
 import Tabs   from '~/components/Tabs'
 
-import { Container } from './styles'
+import { Container, Content, Card, CardContent, CardHeader, CardFooter, Title, Description, Annotation } from './styles'
 
 export default function Main () {
+  let offset = 0
+  const translateY = new Animated.Value(0)
+
+  const animatedEvent = Animated.event([
+    {
+      nativeEvent: {
+        translationY: translateY
+      }
+    }
+  ], { useNativeDriver: true })
+
+  function onHandlerStateChanged (event) {
+    if (event.nativeEvent.oldState === State.ACTIVE){
+      let opened = false
+      const { translationY } = event.nativeEvent
+      offset += translationY
+      translateY.setOffset(offset)
+      translateY.setValue(0)
+
+      if (translationY >= 100) {
+        opened = true
+      } else {
+        translateY.setOffset(0)
+        translateY.setValue(offset)
+        offset = 0
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true
+      }).start(() => {
+        offset = opened ? 380 : 0
+        translateY.setOffset(offset)
+        translateY.setValue(0)
+      })
+    }
+  }
+
   return (
     <Container>
       <Header/>
-      <Tabs/>
+
+      <Content>
+
+        <Menu translateY={translateY}/>
+
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}
+        >
+          <Card style={{
+              transform: [{ translateY: translateY.interpolate({
+                  inputRange: [-350, 0, 380],
+                  outputRange: [-50, 0, 380],
+                  extrapolate: 'clamp'
+                }) }]
+            }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 197.611,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$20,00 recebida hoje de Samuel Terra hoje às 06:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+
+      </Content>
+
+      <Tabs translateY={translateY}/>
     </Container>
   )
 }
